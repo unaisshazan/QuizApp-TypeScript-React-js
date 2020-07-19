b158201e905c2cc262ad18a9c56801217baa6234
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-19 21:06:01 +0500

    add

--- typescript/src/sample.js
+++ typescript/src/sample.js
@@ -1,5 +1,17 @@
 console.log("hellow");
-var a = 1;
-a = true;
-a = [1, 2, 3];
-console.log(a);
+// let a =1;
+// a=true;
+// a1=[1,2,3]
+// console.log(a)
+// let std ={};
+// console.log(std.name)
+// function a(b){
+//     return b/2
+// }
+// //a(2)
+// a("z");
+function a(b) {
+    return b / 2;
+}
+//a(2)
+a("z");
--- typescript/src/sample.ts
+++ typescript/src/sample.ts
@@ -1,2 +1,25 @@
 console.log("hellow");
+// let a =1;
+// a=true;
+// a1=[1,2,3]
+// console.log(a)
 
+    // let std ={};
+    // console.log(std.name)
+
+    // function a(b){
+
+
+    //     return b/2
+    // }
+    // //a(2)
+    // a("z");
+
+    
+    function a(b:number) { //x:type
+
+
+        return b / 2
+    }
+    //a(2)
+    a(2);
