c58af79abe34c0207b497ca3e2edde16c6072a16
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-19 22:30:58 +0500

    add .

--- typescript/build/sample2.js
+++ typescript/build/sample2.js
@@ -1,2 +1,7 @@
 "use strict";
 var isActive = true;
+document.write("hello from Ts");
+function isEven(num) {
+    return num % 2 == 0;
+}
+console.log(isEven(10));
--- typescript/src/sample2.ts
+++ typescript/src/sample2.ts
@@ -1,1 +1,8 @@
 const isActive : boolean = true;
+document.write("hello from Ts");
+function isEven(num:number){
+
+    return num % 2==0;
+}
+
+console.log(isEven(10))
