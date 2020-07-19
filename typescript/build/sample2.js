68cba941f73956aa948deb7e649a499a78d2fe10
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-19 22:32:57 +0500

    Update sample2.js

--- typescript/build/sample2.js
+++ typescript/build/sample2.js
@@ -1,7 +1,11 @@
 "use strict";
 var isActive = true;
 document.write("hello from Ts");
-function isEven(num) {
-    return num % 2 == 0;
-}
-console.log(isEven(10));
+// function isEven(num:number){
+//     return num % 2==0;
+// }
+// console.log(isEven(10))
+var a = 1;
+a = true;
+a = [1, 2, 3];
+console.log(a);
