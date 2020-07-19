43cdece224047db2471b3ec4cf99cdd690982fb9
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-19 21:54:02 +0500

    Create sample.js

---
+++ typescript/build/sample.js
@@ -0,0 +1,25 @@
+"use strict";
+console.log("hellow");
+//just uncomment and enjoy
+// let a =1;
+// a=true;
+// a1=[1,2,3]
+// console.log(a)
+// let std ={};
+// console.log(std.name)
+// function a(b){
+//     return b/2
+// }
+// //a(2)
+// a("z");
+// function a(b : number) { //x:type
+//     return b / 2
+// }
+// //a(2)
+// a(2);
+// let firstName: string = "Unais";
+// let lastName: string = "ALi";
+// let age: number = 20;
+// let isActiveStudent: boolean = true; 
+// let courses: string[]=["TS","JS","React"]; //array method array[]=['a','b','c']
+// let fullname: number=firstName + age; //error number + string not posible
