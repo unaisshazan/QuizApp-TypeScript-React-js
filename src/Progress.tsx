0d2267af480ca24c2a906c16c90f4a79d792478b
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-21 00:28:28 +0500

    Update Progress.tsx

--- src/Progress.tsx
+++ src/Progress.tsx
@@ -2,19 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-      
-      
-    display: 'flex',
-    '& > * + *': {
-      marginLeft: theme.spacing(2),
-    },
-  },
-}));
-
 export default function CircularIndeterminate() {
-  const classes = useStyles();
 
   return (
     <div  style={{alignItems:'center', textAlign:'center',marginTop:'300px'}}>
