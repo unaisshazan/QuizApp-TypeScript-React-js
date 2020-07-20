2bb2debf1b7c40234b775fc9fbd60eedfa5c3c95
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-20 21:25:12 +0500

    add

--- src/quiz_services.ts
+++ src/quiz_services.ts
@@ -1,6 +1,8 @@
 
 import {Quiz} from './../types/quiz_types';
 
+export const shuffleArray=(array: any[])=>
+[...array].sort(()=> Math.random() - 0.5)
 export const  getQuizDetails=async (totalQuestions: number,level: string):Promise<Quiz[]>=>{
  const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`)
     let  {results} = await res.json();
@@ -9,7 +11,7 @@ export const  getQuizDetails=async (totalQuestions: number,level: string):Promis
 return{
     question:questionObj,
     answer:questionObj.correct_answer,
-    Option: questionObj.incorrect_answers.concat(questionObj.correct_answer)
+    Option: shuffleArray(questionObj.incorrect_answers.concat(questionObj.correct_answer))
 }
 
     })
