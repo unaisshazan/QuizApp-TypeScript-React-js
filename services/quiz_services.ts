e967ca90b5a153304b1588d8340076fb9bf7359e
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-20 19:43:32 +0500

    add .

---
+++ services/quiz_services.ts
@@ -0,0 +1,5 @@
+async function getQuizDetails(totalQuestions: number,level: string){
+ const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`)
+    let data = await res.json();
+    console.log(data)
+}
