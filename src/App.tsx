4860b72981ec10ecb54732a8de6e36df05b22c67
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-20 21:49:15 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React,{useEffect,useState} from 'react';
 import {getQuizDetails} from './quiz_services';
 import QuestionCard from './components/QuestionCard';
 import './App.css';
-import { Quiz } from './types/quiz_types';
+import { QuestionType } from './types/quiz_types';
 
 function App() {
-  let [quiz,setQuize]=useState<Quiz[]>([])
+  let [quiz,setQuize]=useState<QuestionType[]>([])
   useEffect(() => {
     
  async function fetchData(){
-   const questions:Quiz[]= await getQuizDetails(5,'easy'); 
+   const questions:QuestionType[]= await getQuizDetails(5,'easy'); 
    console.log(questions);
    setQuize(questions)
  }
@@ -18,7 +18,12 @@ function App() {
 
   return (
     <div className="App">
-        <QuestionCard/>
+        <QuestionCard
+        options={quiz[0].option:string[]}
+        question={}
+        
+        
+        />
   
     </div>
   );
