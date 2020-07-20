dfcf06f2ab0e0eca5c6ee5e33dcbd2f744e06acf
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-20 20:32:32 +0500

    add .

--- src/quiz_services.ts
+++ src/quiz_services.ts
@@ -1,7 +1,17 @@
-import { resourceUsage } from "process";
 
-export const  getQuizDetails=async (totalQuestions: number,level: string)=>{
+import {Quiz,QuestionType} from './../types/quiz_types';
+
+export const  getQuizDetails=async (totalQuestions: number,level: string):Promise<Quiz[]>=>{
  const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`)
-    let {results} = await res.json();
-  return results;
+    let  {results} = await res.json();
+    const quiz = results.map((questionObj:Quiz,ind: number)=>
+    {
+return{
+    question:questionObj,
+    answer:questionObj.correct_answer,
+    Option: questionObj.incorrect_answers.concat(questionObj.correct_answer)
+}
+
+    })
+    return quiz;
 }
--- types/quiz_types.ts
+++ types/quiz_types.ts
@@ -1,8 +1,16 @@
-export type QuestionType={
+import { type } from "os"
+
+export type Quiz={
 category: string
 correct_answer: string
 difficulty: string
 incorrect_answers: string[]
 question: string
 type: string
+}
+export type QuestionType={
+
+ question:string
+ answer:string
+ Option:string[]  
 }
