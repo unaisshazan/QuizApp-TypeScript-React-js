13e6687b4c953914141657870066f6a46d3241bb
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-20 23:43:56 +0500

    Update Progress.tsx

--- src/Progress.tsx
+++ src/Progress.tsx
@@ -1,50 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
-function CircularProgressWithLabel(props) {
+const useStyles = makeStyles((theme) => ({
+  root: {
+      
+      
+    display: 'flex',
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
   return (
-    <Box position="relative" display="inline-flex">
-      <CircularProgress variant="static" {...props} />
-      <Box
-        top={0}
-        left={0}
-        bottom={0}
-        right={0}
-        position="absolute"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Typography variant="caption" component="div" color="textSecondary">{`${Math.round(
-          props,
-        )}%`}</Typography>
-      </Box>
-    </Box>
+    <div  style={{alignItems:'center', textAlign:'center',marginTop:'300px'}}>
+    
+      <CircularProgress color="secondary" />
+    </div>
   );
-}
-
-CircularProgressWithLabel.propTypes = {
-  /**
-   * The value of the progress indicator for the determinate and static variants.
-   * Value between 0 and 100.
-   */
-  value: PropTypes.number.isRequired,
-};
-
-export default function CircularStatic() {
-  const [progress, setProgress] = React.useState(10);
-
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 10));
-    }, 800);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  return <CircularProgressWithLabel value={progress} />;
 }
