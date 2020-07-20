91da9dc63cf42ea2a9720796ba0190b0f0a1935f
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-20 21:21:35 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import {getQuizDetails} from './quiz_services';
 
 import './App.css';
+import { Quiz } from '../types/quiz_types';
 
 function App() {
+  let [quiz,setQuize]=useState<Quiz[]>([])
   useEffect(() => {
     
  async function fetchData(){
-   const questions= await getQuizDetails(5,'easy'); 
-   console.log(questions)
+   const questions:Quiz[]= await getQuizDetails(5,'easy'); 
+   console.log(questions);
+   setQuize(questions)
  }
  fetchData();
   }, []);
